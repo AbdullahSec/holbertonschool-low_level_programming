cbfa6f3746aa7d99bad38ba751b7e9e3a7735ed9
AbdullahSec <Talonkali@outlook.com>
2026-07-08 17:27:26 +0300

    6-size.c finished

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return 0 Sucess
+*/
+
+int main(void)
+{
+printf("Size of a char, %lu, byte(s)\n", sizeof(float));
+printf("Size of a char, %lu, byte(s)\n", sizeof(long long int));
+printf("Size of a char, %lu, byte(s)\n", sizeof(long int));
+printf("Size of a char, %lu, byte(s)\n", sizeof(int));
+return (0);
+}
