b36a50b6039ad4af53cbb048497220f2a6032932
AbdullahSec <Talonkali@outlook.com>
2026-07-11 17:11:51 +0300

    fixed 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,52 +1,50 @@
 #include "main.h"
 
 /**
+ * print_num - prints a number padded to 3 spaces
+ * @p: the number to print
+ */
+void print_num(int p)
+{
+	if (p < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(p + '0');
+	}
+	else if (p < 100)
+	{
+		_putchar(' ');
+		_putchar((p / 10) + '0');
+		_putchar((p % 10) + '0');
+	}
+	else
+	{
+		_putchar((p / 100) + '0');
+		_putchar(((p / 10) % 10) + '0');
+		_putchar((p % 10) + '0');
+	}
+}
+
+/**
  * print_times_table - prints the n times table
  * @n: the table number
  */
 void print_times_table(int n)
 {
-	int i, j, p;
+	int i, j;
 
 	if (n < 0 || n > 15)
 		return;
-	i = 0;
-	while (i <= n)
+	for (i = 0; i <= n; i++)
 	{
-		j = 0;
-		while (j <= n)
+		_putchar('0');
+		for (j = 1; j <= n; j++)
 		{
-			p = i * j;
-			if (j == 0)
-			{
-				_putchar(p + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (p < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p + '0');
-				}
-				else if (p < 100)
-				{
-					_putchar(' ');
-					_putchar((p / 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-				else
-				{
-					_putchar((p / 100) + '0');
-					_putchar(((p / 10) % 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-			}
-			j++;
+			_putchar(',');
+			_putchar(' ');
+			print_num(i * j);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
