1746a7e98b9616bbfb2ca278b9f38293de6a487e
AbdullahSec <Talonkali@outlook.com>
2026-07-14 18:36:31 +0300

    fixed betty

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 
-/* reset_to_98 means make the pointer 98
-  8 *
-  9 * @n: number to check
-    *
- 10 * Return : void
- 11 */
+/*
+ * reset_to_98 means make the pointer 98
+ *
+ * @n: number to check
+ *
+ * Return : void
+ */
 
 
 
