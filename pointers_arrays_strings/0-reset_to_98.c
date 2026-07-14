f2d9a6207a681b9ff54d2f1ef5d9c0ee0b0712a7
AbdullahSec <Talonkali@outlook.com>
2026-07-14 18:33:52 +0300

    finished 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/* reset_to_98 means make the pointer 98
+  8 *
+  9 * @n: number to check
+    *
+ 10 * Return : void
+ 11 */
+
+
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+
+
+
+
+}
