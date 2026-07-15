5c2bd43c689c527ba488d27c228da8870ab677bf
AbdullahSec <Talonkali@outlook.com>
2026-07-15 17:50:11 +0300

    finshed 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* swap_int - is used to swap betwwen two pointers
+*
+* Return : void
+*
+* @a: has a value of 98
+*
+* @b: has a value of 42
+**/
+
+
+void swap_int(int *a, int *b)
+{
+
+int tmp;
+tmp = 0
+
+tmp = *a;
+*a = *b;
+*b = tmp;
+
+}
