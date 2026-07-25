2a476240ae0b5bac554af871b2db3e88819f74b0
AbdullahSec <Talonkali@outlook.com>
2026-07-25 14:24:20 +0300

    Refactor 101-mul: split multiply, pass Betty

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -48,6 +48,32 @@ void errors(void)
 }
 
 /**
+ * multiply - multiplies two number strings into a digit array
+ * @s1: first number string
+ * @s2: second number string
+ * @result: zero-filled array that receives the product digits
+ */
+void multiply(char *s1, char *s2, int *result)
+{
+	int i, j, carry, d1, d2;
+
+	for (i = _strlen(s1) - 1; i >= 0; i--)
+	{
+		d1 = s1[i] - '0';
+		carry = 0;
+		for (j = _strlen(s2) - 1; j >= 0; j--)
+		{
+			d2 = s2[j] - '0';
+			carry += result[i + j + 1] + (d1 * d2);
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[i + j + 1] += carry;
+	}
+}
+
+/**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -56,36 +82,15 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len, i, *result, a = 0;
 
 	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 		errors();
-
-	s1 = argv[1];
-	s2 = argv[2];
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2;
+	len = _strlen(argv[1]) + _strlen(argv[2]);
 	result = calloc(len, sizeof(int));
 	if (!result)
 		return (1);
-
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
-	}
-
+	multiply(argv[1], argv[2], result);
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
@@ -96,6 +101,5 @@ int main(int argc, char *argv[])
 	_putchar(result[len - 1] + '0');
 	_putchar('\n');
 	free(result);
-
 	return (0);
 }
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,10 +2,15 @@
 #define MAIN_H
 
 int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 int is_digit(char *s);
 int _strlen(char *s);
 void errors(void);
-void multiply(char *s1, char *s2, int *result, int len);
-void print_result(int *result, int len);
+void multiply(char *s1, char *s2, int *result);
 
 #endif /* MAIN_H */
