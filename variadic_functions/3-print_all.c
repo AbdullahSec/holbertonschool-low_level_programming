bdbb9887025147bb8eeb0ade9fc1d7f97ce18a3e
AbdullahSec <Talonkali@outlook.com>
2026-08-01 17:19:36 +0300

    Refactor task 3 to use function pointers: max 2 if, 2 while

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,37 +2,79 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - prints a char argument
+ * @args: the variadic arguments list
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int argument
+ * @args: the variadic arguments list
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float argument
+ * @args: the variadic arguments list
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string argument, "(nil)" if NULL
+ * @args: the variadic arguments list
+ */
+void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
  * print_all - prints anything, based on a format string
  * @format: list of types of arguments (c, i, f, s); others ignored
  */
 void print_all(const char * const format, ...)
 {
-	int i;
-	char *str;
+	int i, j;
 	char *sep;
 	va_list args;
+	printer_t printers[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
 	i = 0;
 	sep = "";
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 'c')
-			printf("%s%c", sep, va_arg(args, int));
-		if (format[i] == 'i')
-			printf("%s%d", sep, va_arg(args, int));
-		if (format[i] == 'f')
-			printf("%s%f", sep, va_arg(args, double));
-		if (format[i] == 's')
+		j = 0;
+		while (printers[j].symbol != NULL)
 		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
+			if (format[i] == printers[j].symbol[0])
+			{
+				printf("%s", sep);
+				printers[j].print(args);
+				sep = ", ";
+			}
+			j++;
 		}
-		if (format[i] == 'c' || format[i] == 'i' ||
-			format[i] == 'f' || format[i] == 's')
-			sep = ", ";
 		i++;
 	}
 	va_end(args);
